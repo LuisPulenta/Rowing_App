1ad250c08c8e7361e4672e7e68dde5fc8d9b5245
LuisPulenta <luisalbertonu@gmail.com>
2022-05-09 19:30:06 -0300

    Inspeccion

--- lib/screens/inspeccion_cuestionario_screen.dart
+++ lib/screens/inspeccion_cuestionario_screen.dart
@@ -24,6 +24,10 @@ class _InspeccionCuestionarioScreenState
 //*****************************************************************************
 
   List _elements = [];
+  int puntos = 0;
+  int respSI = 0;
+  int respNO = 0;
+  int respNA = 0;
 
 //*****************************************************************************
 //************************** INIT STATE ***************************************
@@ -76,7 +80,8 @@ class _InspeccionCuestionarioScreenState
         _showResultado(),
         Expanded(
           child: _getListView(),
-        )
+        ),
+        _showButtonsGuardarCancelar(),
       ],
     );
   }
@@ -88,21 +93,163 @@ class _InspeccionCuestionarioScreenState
   Widget _showResultado() {
     return Container(
       padding: EdgeInsets.all(10),
-      height: 40,
+      height: 80,
       child: Row(
         children: [
-          Text("Puntaje: ",
-              style: TextStyle(
-                fontSize: 14,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              )),
-          Text('1000',
-              style: TextStyle(
-                fontSize: 14,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              )),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text("Preguntas: ",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  Text(widget.detallesFormulariosCompleto.length.toString(),
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.blue,
+                        fontWeight: FontWeight.normal,
+                      )),
+                ],
+              ),
+              Row(
+                children: [
+                  Text("Resp SI: ",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  Text(respSI.toString(),
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.blue,
+                        fontWeight: FontWeight.normal,
+                      )),
+                ],
+              ),
+              Row(
+                children: [
+                  Text("Faltan Responder: ",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  Text(
+                      (widget.detallesFormulariosCompleto.length -
+                              respSI -
+                              respNO -
+                              respNA)
+                          .toString(),
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.blue,
+                        fontWeight: FontWeight.normal,
+                      )),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text("",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  Text("",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.blue,
+                        fontWeight: FontWeight.normal,
+                      )),
+                ],
+              ),
+              Row(
+                children: [
+                  Text("Resp. NO: ",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  Text(respNO.toString(),
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.blue,
+                        fontWeight: FontWeight.normal,
+                      )),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text("",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  Text("",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.blue,
+                        fontWeight: FontWeight.normal,
+                      )),
+                ],
+              ),
+              Row(
+                children: [
+                  Text("Resp. N/A: ",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  Text(respNA.toString(),
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.blue,
+                        fontWeight: FontWeight.normal,
+                      )),
+                ],
+              ),
+              Row(
+                children: [
+                  Text("Puntos: ",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  Text(puntos.toString(),
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.blue,
+                        fontWeight: FontWeight.normal,
+                      )),
+                ],
+              ),
+            ],
+          ),
         ],
       ),
     );
@@ -134,6 +281,11 @@ class _InspeccionCuestionarioScreenState
           elevation: 8.0,
           margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
           child: Container(
+            color: (element['cumple'] != "SI" &&
+                    element['cumple'] != "NO" &&
+                    element['cumple'] != "N/A")
+                ? Colors.white
+                : Colors.blue[400],
             child: Column(
               children: [
                 ListTile(
@@ -189,11 +341,32 @@ class _InspeccionCuestionarioScreenState
                             ),
                           ),
                           onPressed: () {
+                            if (element['cumple'] == "NO") {
+                              respNO--;
+                              respSI++;
+                              puntos = puntos +
+                                  int.parse(element['ponderacionpuntos']);
+                            }
+                            if (element['cumple'] == "N/A") {
+                              respNA--;
+                              respSI++;
+                              puntos = puntos +
+                                  int.parse(element['ponderacionpuntos']);
+                            }
+                            if (element['cumple'] != "SI" &&
+                                element['cumple'] != "NO" &&
+                                element['cumple'] != "N/A") {
+                              respSI++;
+                              puntos = puntos +
+                                  int.parse(element['ponderacionpuntos']);
+                            }
+
                             _elements.forEach((e) {
                               if (e == element) {
                                 e['cumple'] = "SI";
                               }
                             });
+
                             setState(() {});
                           },
                         ),
@@ -218,6 +391,21 @@ class _InspeccionCuestionarioScreenState
                             ),
                           ),
                           onPressed: () {
+                            if (element['cumple'] == "SI") {
+                              respSI--;
+                              respNO++;
+                              puntos = puntos -
+                                  int.parse(element['ponderacionpuntos']);
+                            }
+                            if (element['cumple'] == "N/A") {
+                              respNA--;
+                              respNO++;
+                            }
+                            if (element['cumple'] != "SI" &&
+                                element['cumple'] != "NO" &&
+                                element['cumple'] != "N/A") {
+                              respNO++;
+                            }
                             element['cumple'] = "NO";
                             setState(() {});
                           },
@@ -243,6 +431,21 @@ class _InspeccionCuestionarioScreenState
                             ),
                           ),
                           onPressed: () {
+                            if (element['cumple'] == "SI") {
+                              respSI--;
+                              respNA++;
+                              puntos = puntos -
+                                  int.parse(element['ponderacionpuntos']);
+                            }
+                            if (element['cumple'] == "NO") {
+                              respNO--;
+                              respNA++;
+                            }
+                            if (element['cumple'] != "SI" &&
+                                element['cumple'] != "NO" &&
+                                element['cumple'] != "N/A") {
+                              respNA++;
+                            }
                             element['cumple'] = "N/A";
                             setState(() {});
                           },
@@ -256,6 +459,75 @@ class _InspeccionCuestionarioScreenState
           ),
         );
       },
+    );
+  }
+
+//*****************************************************************************
+//************************** SHOWBUTTONSGUARDARCANCELAR ***********************
+//*****************************************************************************
+
+  Widget _showButtonsGuardarCancelar() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Expanded(
+                child: ElevatedButton(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.save),
+                        SizedBox(
+                          width: 2,
+                        ),
+                        Text('Guardar', style: TextStyle(fontSize: 12)),
+                      ],
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xFF282886),
+                      minimumSize: Size(double.infinity, 40),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                    onPressed: () {
+                      //_guardar();
+                    }),
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.cancel),
+                      SizedBox(
+                        width: 2,
+                      ),
+                      Text('Cancelar', style: TextStyle(fontSize: 12)),
+                    ],
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xffdf281e),
+                    minimumSize: Size(double.infinity, 40),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context, "No");
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
