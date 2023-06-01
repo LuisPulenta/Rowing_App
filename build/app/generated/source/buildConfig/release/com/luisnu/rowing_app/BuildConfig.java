94fe0b3bb8addc3b9ffa2dfee743546acc19f2be
LuisPulenta <luisalbertonu@gmail.com>
2023-06-01 12:05:23 -0300

    V1.0.43

--- build/app/generated/source/buildConfig/release/com/luisnu/rowing_app/BuildConfig.java
+++ build/app/generated/source/buildConfig/release/com/luisnu/rowing_app/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.luisnu.rowing_app";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 42;
-  public static final String VERSION_NAME = "1.0.42";
+  public static final int VERSION_CODE = 43;
+  public static final String VERSION_NAME = "1.0.43";
 }
