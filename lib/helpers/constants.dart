8199dbc519179be4e591b2219f6f2c8995a66c56
Luis Núñez <luisalbertonu@gmail.com>
2025-02-14 10:31:07 -0300

    V77

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -1,7 +1,7 @@
 class Constants {
   static String get apiUrl => 'http://190.111.249.225/RowingAppApi';
 
-  static String get version => '1.0.76';
+  static String get version => '1.0.77';
 
   static bool get grabarCoord => true;
 }
